8dd21a2745d5e84e5fc8c40883c1a336ea82fcd1
Linda Chang <lindachang186@gmail.com>
2018-11-13 21:46:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,18 +2,28 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artist = []
+  @@artists = []
   @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << @genre
+    @@artists << @artist
     @@count +=1
   end
 
   def self.count
     @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
   end
 
 end
